32884c85d34bf27be92eba74a26693626535c7e5
VaneChu <i@mabudao.com>
2014-10-20 02:10:29 +0900

    ex_5 fixed

--- codes/programming_5_train.py
+++ codes/programming_5_train.py
@@ -8,6 +8,8 @@ class HMM(object):
    def __init__(self):
       self.trans_prob = defaultdict(lambda:0)
       self.emiss_prob = defaultdict(lambda:0)
+      self.tags = set([])
+      self.words = set([])
 
    def train(self, corpus):
       emiss = defaultdict(lambda:0)
@@ -18,6 +20,8 @@ class HMM(object):
             previous = '<s>'
             for word_tag in line.strip().split(' '):
                [word, tag] = word_tag.split('_')
+               self.words.add(word)
+               self.tags.add(tag)
                trans[previous + ' ' + tag] += 1
                emiss[tag + ' ' + word] += 1
                context[previous] += 1 
@@ -25,61 +29,89 @@ class HMM(object):
             context[tag] += 1   # for the last word /<s>
             trans[tag + ' ' + '/<s>'] += 1
       for transition, count in trans.items():
-         self.trans_prob[transition] = float(count) / context[transition.split(" ")[0]] # no smothing
+         self.trans_prob[transition] = str(float(count) / context[transition.split(" ")[0]]) # no smothing
       for emission, count in emiss.items():
-         self.emiss_prob[emission] = float(count) / context[emission.split(" ")[0]] 
-      # pprint.pprint(self.emiss_prob)
+         self.emiss_prob[emission] = str(float(count) / context[emission.split(" ")[0]])
+      pprint.pprint(self.emiss_prob)
+      pprint.pprint(self.trans_prob)
 
    def load_model(self, model):
       with open(model, "r") as f:
          for line in f.readlines():
-            if line.split(" ")[0] == 'T':
-               self.trans_prob[" ".join(line.split(" ")[1:3])] = line.strip().split(" ")[3]
-            elif line.split(" ")[0] == 'E':
-               self.emiss_prob[" ".join(line.split(" ")[1:3])] = line.strip().split(" ")[3]
+            T_or_E = line.strip().split(" ")[0]
+            prev = line.strip().split(" ")[1]
+            next = line.strip().split(" ")[2] # tag or word included
+            prob = line.strip().split(" ")[3]
+            pair_tag = " ".join(line.split(" ")[1:3])
 
-   def forword_backword(self):
-      pass
+            if T_or_E == 'T':   
+               self.trans_prob[pair_tag] = prob
+               self.tags.add(prev)
+               self.tags.add(next)
+            elif T_or_E == 'E':
+               self.emiss_prob[pair_tag] = prob
+               self.words.add(next)
+      print "load:"
+      pprint.pprint(self.emiss_prob)
+      pprint.pprint(self.trans_prob)
+      # print self.tags
+      # print self.words
 
-def hmm_train(model):
-   unigram = defaultdict(lambda:0)
-   for line in model.readlines():
-      unigram[line.split()[0]] = float(line.split()[1])
-   return unigram
+   def smooth(self):
+      para_lambda = 0.95
+      N = len(self.words)
 
-def forward_backward(input_file):
-   INF = 10 ** 10
-   lam = 0.95
-   lam_unk = 1 - lam
-   V = 10 ** 6
-   for line in input_file.readlines():
-      line = line.strip()
-      ary_size = len(line) + 1
-      best_edge = [None] * ary_size
-      best_score = [0.0] * ary_size
-      for word_end in range(1, ary_size):
-         best_score[word_end] = INF
-         for word_begin in range(0, word_end):
-            word = line[word_begin: word_end]
-            if (word in unigram) :
-               prob = unigram[word] / float(V)
-               prob += unigram[word] * lam_unk
-               my_score = best_score[word_begin] - float(log(prob, 2))
-               if my_score < best_score[word_end]:
-                  best_score[word_end] = my_score
-                  best_edge[word_end] = (word_begin, word_end)
-      words = []
-      next_edge = best_edge[len(best_edge) - 1]
-      while next_edge is not None:
-         word = line[next_edge[0] : next_edge[1]]
-         words.append(word)
-         next_edge = best_edge[next_edge[0]]
-      words.reverse()
-      print words
-      print " ".join(words)
+      for tag in self.tags:
+         for word in self.words:
+            pair = tag + " " + word
+            if pair in self.emiss_prob:
+               self.emiss_prob[pair] = para_lambda * float(self.emiss_prob[pair]) + (1 - para_lambda) * (1.0 / N)
+            else:
+               self.emiss_prob[pair] = (1 - para_lambda) * (1.0 / N)
+      # pprint.pprint(self.emiss_prob)
+
+   def forward_backward(self, input_file):
+      with open(input_file, 'r') as f:
+         for line in f.readlines():
+            best_score = {}
+            best_edge = {}
+            best_score["0 <s>"]  = 0
+            length = len(line.strip().split())
+            for i in xrange(length):
+               for prev in self.tags:
+                  for next in self.tags:
+                     status_pair = str(i) + " " + prev
+                     next_status_pair = str(i+1) + " " + next
+                     trans_pair = prev + " " + next
+                     emiss_pair = next + " " + line.strip().split()[i]
+                     if status_pair in best_score and trans_pair in self.trans_prob:
+                        score = best_score[status_pair] + -log(float(self.trans_prob[trans_pair])) + -log(float(self.emiss_prob[emiss_pair])) 
+                        if next_status_pair not in best_score or best_score[next_status_pair] > score:
+                           best_score[next_status_pair] = score
+                           best_edge[next_status_pair] = str(i) + " " + prev
+
+            for prev in self.tags:  # for '</s>'
+               status_pair = str(length) + " " + prev
+               trans_pair = prev + " " + "</s>"
+               next_status_pair = str(length+1) + " " + next
+               if status_pair in best_score and trans_pair in self.trans_prob:
+                  score = best_score[status_pair] + -log(float(self.trans_prob[trans_pair]))
+                  if next_status_pair not in best_score or best_score[next_status_pair] > score:
+                     best_score[next_status_pair] = score
+                     best_edge[next_status_pair] = str(length) + " " + prev
+
+            tags = []
+            next_edge = best_edge[str(length+1) + " </s>"]
+            while next_edge != "0 <s>":
+               position, tag = next_edge.split()
+               tags.append(tag)
+               next_edge = best_edge[next_edge]
+            tags.reverse()
+            print " ".join(tags)
 
 # with open(sys.argv[1], "r") as input_file:
 #    unigram = load_model(input_file)
+
 
 # with open(sys.argv[2], "r") as input_file:
 #    forward_backward(input_file)
@@ -88,3 +120,5 @@ if __name__ == "__main__":
    hmm = HMM()
    # hmm.train("../test/05-train-input.txt")
    hmm.load_model("../test/05-train-answer.txt")
+   hmm.smooth()
+   hmm.forward_backward("../test/05-test-input.txt")
